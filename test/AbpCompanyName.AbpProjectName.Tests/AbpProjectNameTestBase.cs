42e2cdfcf49201c8060866aa38def21f9ff4a0f7
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-06-19 18:03:09 +0300

    Added unit test project and a simple test code.

---
+++ test/AbpCompanyName.AbpProjectName.Tests/AbpProjectNameTestBase.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading.Tasks;
+using Abp.Collections;
+using Abp.Modules;
+using Abp.TestBase;
+using AbpCompanyName.AbpProjectName.EntityFrameworkCore;
+using AbpCompanyName.AbpProjectName.Tests.TestDatas;
+
+namespace AbpCompanyName.AbpProjectName.Tests
+{
+    public class AbpProjectNameTestBase : AbpIntegratedTestBase
+    {
+        public AbpProjectNameTestBase()
+        {
+            UsingDbContext(context => new TestDataBuilder(context).Build());
+        }
+
+        protected override void AddModules(ITypeList<AbpModule> modules)
+        {
+            base.AddModules(modules);
+            modules.Add<AbpProjectNameTestModule>();
+        }
+
+        public void UsingDbContext(Action<AbpProjectNameDbContext> action)
+        {
+            using (var context = LocalIocManager.Resolve<AbpProjectNameDbContext>())
+            {
+                action(context);
+                context.SaveChanges();
+            }
+        }
+
+        public T UsingDbContext<T>(Func<AbpProjectNameDbContext, T> func)
+        {
+            T result;
+
+            using (var context = LocalIocManager.Resolve<AbpProjectNameDbContext>())
+            {
+                result = func(context);
+                context.SaveChanges();
+            }
+
+            return result;
+        }
+
+        public async Task UsingDbContextAsync(Func<AbpProjectNameDbContext, Task> action)
+        {
+            using (var context = LocalIocManager.Resolve<AbpProjectNameDbContext>())
+            {
+                await action(context);
+                await context.SaveChangesAsync(true);
+            }
+        }
+
+        public async Task<T> UsingDbContextAsync<T>(Func<AbpProjectNameDbContext, Task<T>> func)
+        {
+            T result;
+
+            using (var context = LocalIocManager.Resolve<AbpProjectNameDbContext>())
+            {
+                result = await func(context);
+                context.SaveChanges();
+            }
+
+            return result;
+        }
+    }
+}
