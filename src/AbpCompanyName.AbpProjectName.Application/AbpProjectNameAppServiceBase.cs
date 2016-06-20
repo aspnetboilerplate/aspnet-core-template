94ec0a4fd3e34aa6a7a8f6742cc5b1db913a1140
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-06-20 10:14:47 +0300

    Removed unnecessary IObjectMapper injection.

--- src/AbpCompanyName.AbpProjectName.Application/AbpProjectNameAppServiceBase.cs
+++ src/AbpCompanyName.AbpProjectName.Application/AbpProjectNameAppServiceBase.cs
@@ -1,5 +1,4 @@
 ﻿using Abp.Application.Services;
-using Abp.ObjectMapping;
 
 namespace AbpCompanyName.AbpProjectName
 {
@@ -8,15 +7,8 @@ namespace AbpCompanyName.AbpProjectName
     /// </summary>
     public abstract class AbpProjectNameAppServiceBase : ApplicationService
     {
-        /// <summary>
-        /// Reference to the object to object mapper.
-        /// TODO: REMOVE THIS WHEN IT'S ADDED TO ApplicationService in Abp.
-        /// </summary>
-        public IObjectMapper ObjectMapper { get; set; }
-        
         protected AbpProjectNameAppServiceBase()
         {
-            ObjectMapper = NullObjectMapper.Instance;
             LocalizationSourceName = AbpProjectNameConsts.LocalizationSourceName;
         }
     }
