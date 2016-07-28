b536a23ae1b1255902e128a26c4a2137d26450fb
myo myint han <myo myint han>
2016-07-28 22:35:17 +0800

    merger

---
+++ src/AbpCompanyName.AbpProjectName.EntityFrameworkCore/Migrations/20160724065003_initial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AbpCompanyName.AbpProjectName.EntityFrameworkCore.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
