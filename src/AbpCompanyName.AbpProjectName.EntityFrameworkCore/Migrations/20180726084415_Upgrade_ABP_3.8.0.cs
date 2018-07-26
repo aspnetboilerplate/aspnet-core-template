484083feb565722816f839e841ddf49aa312c0ab
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2018-07-26 11:47:42 +0300

    Reolved #64 - Upgrade ABP to v3.8

---
+++ src/AbpCompanyName.AbpProjectName.EntityFrameworkCore/Migrations/20180726084415_Upgrade_ABP_3.8.0.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AbpCompanyName.AbpProjectName.Migrations
+{
+    public partial class Upgrade_ABP_380 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
